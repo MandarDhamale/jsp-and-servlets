b846f12d545706bc3c0c0c0b4de1bc4c01cf7761
Mandar Dhamale <mandardhamale@gmail.com>
2025-01-07 13:40:35 +0530

    User logout using session and organizing redirects

--- User_logout_using_sessions/src/main/java/com/mandar/SiteController.java
+++ User_logout_using_sessions/src/main/java/com/mandar/SiteController.java
@@ -2,10 +2,8 @@ package com.mandar;
 
 import java.io.IOException;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
